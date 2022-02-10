7c1a6c1ed57dc65f4bbb9a8b3df659c601123f38
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-10 22:04:32 +0200

    phone2: fingerprint: Add UDFPS extension library
     * Add support for TARGET_USES_FOD_ZPOS.
    
    Change-Id: I49891f89350839ff93ff1a762a83996bfd56ec18

--- device.mk
+++ device.mk
@@ -56,6 +56,8 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3.vendor \
     android.hardware.biometrics.fingerprint@2.3-service.nothing
 
+TARGET_USES_FOD_ZPOS := true
+
 # Camera
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.concurrent.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.concurrent.xml \

50c15436bd1f7608eacb423f678daed5ce68cc7d
Jake Weinstein <jake@aospa.co>
2022-07-19 16:44:38 -0500

    phone2: BoardConfig: Use fstab.qcom as recovery.fstab
    Why duplicate?
    
    Change-Id: Ie3f9227c43de6933020677e210e2e596af355dd3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,7 +60,7 @@ BOARD_EXT4_SHARE_DUP_BLOCKS := true
 TARGET_NO_RECOVERY := true
 # Defines for enabling A/B builds
 AB_OTA_UPDATER := true
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/fstab.qcom
 
 ifeq ($(BOARD_AVB_ENABLE), true)
     BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem

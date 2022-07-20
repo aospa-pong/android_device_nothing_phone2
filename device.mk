0a941d764af7204debbaee189972b43801ef61e1
Jake Weinstein <jake@aospa.co>
2022-07-19 20:52:03 -0500

    phone2: Move vbmeta_system flags to BoardConfig
    Consistent with recovery AVB flags
    
    Change-Id: I225791e1d023ea31f83af6551489b1f9d8f59be2

--- device.mk
+++ device.mk
@@ -77,11 +77,6 @@ PRODUCT_PACKAGES += fastbootd
 PRODUCT_PACKAGES += android.hardware.fastboot@1.1-impl-mock
 
 PRODUCT_COPY_FILES += $(DEVICE_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
-BOARD_AVB_VBMETA_SYSTEM := system system_ext product
-BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
-BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
-BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
 
 $(call inherit-product, build/make/target/product/gsi_keys.mk)
 

76bb448824544437ec14e35ad406738d3ad6b22b
Jake Weinstein <jake@aospa.co>
2022-07-19 20:36:43 -0500

    phone2: BoardConfig: Drop USE_OPENGL_RENDERER
    This has been deprecated for 8 years!
    
    Change-Id: Iab1c25f03867c7db1629bd9a139ed242a58f5be6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,8 +30,6 @@ BOARD_RAMDISK_USE_LZ4 := true
 
 SECTOOLS_SECURITY_PROFILE := $(QCPATH)/securemsm/security_profiles/waipio_tz_security_profile.xml $(QCPATH)/securemsm/security_profiles/fillmore_tz_security_profile.xml $(QCPATH)/securemsm/security_profiles/cape_tz_security_profile.xml
 
-USE_OPENGL_RENDERER := true
-
 # TODO: Enable it back when we have a path forward
 # Disable generation of dtbo.img
 BOARD_KERNEL_SEPARATED_DTBO := false

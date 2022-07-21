593210a85f06309c97d61a364aaf6b2ac94e4f18
Jake Weinstein <jake@aospa.co>
2022-07-20 21:18:25 -0500

    phone2: Makefile: Remove AEP permission
    This is already in the Adreno component.
    
    Change-Id: Ibb01d1f21467896a3694ae56d00a4c0f1cdddf6e

--- device.mk
+++ device.mk
@@ -175,10 +175,6 @@ TARGET_MOUNT_POINTS_SYMLINKS := false
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
 
-#FEATURE_OPENGLES_EXTENSION_PACK support string config file
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
-
 PRODUCT_ENABLE_QESDK := true
 
 # QTI Components

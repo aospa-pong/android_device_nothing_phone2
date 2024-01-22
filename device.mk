0622c134c0b35675ac8b757bc1b29013bb0bc7b2
Kunmun <kunmun@aospa.co>
2024-01-22 15:31:08 +0530

    [DNM]phone2: Don't build fingerprint hal
    Depends on OSS kernel.
    
    Change-Id: I8490c65f43426c595a5ef47e81a0f714822728e4

--- device.mk
+++ device.mk
@@ -50,11 +50,11 @@ BOARD_AVB_ENABLE := true
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3.vendor \
-    android.hardware.biometrics.fingerprint@2.3-service.nothing
+#PRODUCT_PACKAGES += \
+#    android.hardware.biometrics.fingerprint@2.3.vendor \
+#    android.hardware.biometrics.fingerprint@2.3-service.nothing
 
-TARGET_USES_FOD_ZPOS := true
+#TARGET_USES_FOD_ZPOS := true
 
 # Camera
 PRODUCT_COPY_FILES += \
